e96529de007eb48d1b60c5dfe62181cb73b36442
mohamedSabry0 <mohamedsabry3023@gmail.com>
2020-08-19 00:05:24 +0200

    try ignoring example linter errors

---
+++ example.rb
@@ -0,0 +1,12 @@
+#rubocop:disable Lint/Syntax
+class bad_example
+end
+class Bad_Example; end
+class GoodExample
+end
+class BADNAME
+end
+class badname; end
+class GoodName; end
+
+#rubocop:enable Lint/Syntax
