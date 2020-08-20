183df64f546c9212f5cfb17ec1fa198c57f5b454
mohamedSabry0 <mohamedsabry3023@gmail.com>
2020-08-20 08:43:53 +0200

    fix regex to include a bad example

--- lib/naming_conventions.rb
+++ lib/naming_conventions.rb
@@ -21,7 +21,7 @@ module NamingConventions
   def validate_classes(names)
     class_warnings = []
     names[:classes].each do |_key, value|
-      if (value[:name] =~ /([A-Z][a-z]*)+/).nil?
+      if (value[:name] =~ /^([A-Z][a-z]*)+$/).nil?
         class_warnings.push("naming_warning @(#{file_path}, line##{value[:line] + 1}):" \
          "'#{value[:name]}' is not a good class name. Follow CamelCase naming convention\n")
       end
