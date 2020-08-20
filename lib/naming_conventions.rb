97d88c1ad2c5076f4dcf1cbd163a8910dc17b9c4
mohamedSabry0 <mohamedsabry3023@gmail.com>
2020-08-20 09:23:29 +0200

    start working on variables

--- lib/naming_conventions.rb
+++ lib/naming_conventions.rb
@@ -3,12 +3,15 @@ module NamingConventions
     file.each_with_index do |line, index|
       matched = line =~ /[^"']=/
       if !(line =~ /[^"']=/).nil?
-        variable = line[0...matched].split(',')
-        names[:variables][variable] =
-          if names[:variables].key?(variable)
-            names[:variables][variable][:count] + 1
-          else
-            { count: 1, line: index }
+        variables = line[0..matched].split(',')
+        variables.each do |variable|
+          variable = variable.strip
+          names[:variables][variable] =
+            if names[:variables].key?(variable)
+              names[:variables][variable][:count] + 1
+            else
+              { count: 1, line: index }
+            end
           end
       elsif !(line =~ /^\s*class /).nil?
         name = line[/[^(\s*class )]\w+/]
