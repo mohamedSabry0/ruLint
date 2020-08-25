91459590542c6ad470f693690454675c3180a572
mohamedSabry0 <mohamedsabry3023@gmail.com>
2020-08-25 19:17:15 +0200

    fix rubocop

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,9 +2,30 @@ require_relative '../lib/parser.rb'
 
 describe Parser do
   let(:parser) { Parser.new('../example.rb') }
-  let(names_array) { {:variables=>{"var"=>2, "sec +"=>2}, 
-  :classes=>{"bad_example"=>{:name=>"bad_example", :line=>1}, "bad"=>{:name=>"bad", :line=>4}, "Bad_Example"=>{:name=>"Bad_Example", :line=>6}, "GoodExample"=>{:name=>"GoodExample", :line=>9}, "Mu_Foo"=>{:name=>"Mu_Foo", :line=>14}, "BADNAME"=>{:name=>"BADNAME", :line=>16}, "badname"=>{:name=>"badname", :line=>19}, "GoodName"=>{:name=>"GoodName", :line=>20}},
-  :modules=>{"bad_example"=>{:name=>"bad_example", :line=>22}, "bad"=>{:name=>"bad", :line=>25}, "Bad_Example"=>{:name=>"Bad_Example", :line=>27}, "GoodExample"=>{:name=>"GoodExample", :line=>30}, "Mu_Foo"=>{:name=>"Mu_Foo", :line=>35}, "BADNAME"=>{:name=>"BADNAME", :line=>37}, "badname"=>{:name=>"badname", :line=>40}, "GoodName"=>{:name=>"GoodName", :line=>41}}} }
+  let(:names_array) do
+    { variables: { 'var' => 2, 'sec +' => 2 },
+      classes: {
+        'bad_example' => { name: 'bad_example', line: 1 },
+        'bad' => { name: 'bad', line: 4 },
+        'Bad_Example' => { name: 'Bad_Example', line: 6 },
+        'GoodExample' => { name: 'GoodExample', line: 9 },
+        'Mu_Foo' => { name: 'Mu_Foo', line: 14 },
+        'BADNAME' => { name: 'BADNAME', line: 16 },
+        'badname' => { name: 'badname', line: 19 },
+        'GoodName' => { name: 'GoodName', line: 20 }
+      },
+      modules: {
+        'bad_example' => { name: 'bad_example', line: 22 },
+        'bad' => { name: 'bad', line: 25 },
+        'Bad_Example' => { name: 'Bad_Example', line: 27 },
+        'GoodExample' => { name: 'GoodExample', line: 30 },
+        'Mu_Foo' => { name: 'Mu_Foo', line: 35 },
+        'BADNAME' => { name: 'BADNAME', line: 37 },
+        'badname' => { name: 'badname', line: 40 },
+        'GoodName' => { name: 'GoodName', line: 41 }
+      } }
+  end
+
   describe '#new' do
     it 'initialize and store names hash as accessor' do
       expect(parser.names).to be_kind_of(Hash)
