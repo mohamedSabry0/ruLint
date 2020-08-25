6d981f00bbd534d058088a520c8bd597b11240cf
mohamedSabry0 <mohamedsabry3023@gmail.com>
2020-08-25 18:57:58 +0200

    adding some tests to parser_spec

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,6 +2,9 @@ require_relative '../lib/parser.rb'
 
 describe Parser do
   let(:parser) { Parser.new('../example.rb') }
+  let(names_array) { {:variables=>{"var"=>2, "sec +"=>2}, 
+  :classes=>{"bad_example"=>{:name=>"bad_example", :line=>1}, "bad"=>{:name=>"bad", :line=>4}, "Bad_Example"=>{:name=>"Bad_Example", :line=>6}, "GoodExample"=>{:name=>"GoodExample", :line=>9}, "Mu_Foo"=>{:name=>"Mu_Foo", :line=>14}, "BADNAME"=>{:name=>"BADNAME", :line=>16}, "badname"=>{:name=>"badname", :line=>19}, "GoodName"=>{:name=>"GoodName", :line=>20}},
+  :modules=>{"bad_example"=>{:name=>"bad_example", :line=>22}, "bad"=>{:name=>"bad", :line=>25}, "Bad_Example"=>{:name=>"Bad_Example", :line=>27}, "GoodExample"=>{:name=>"GoodExample", :line=>30}, "Mu_Foo"=>{:name=>"Mu_Foo", :line=>35}, "BADNAME"=>{:name=>"BADNAME", :line=>37}, "badname"=>{:name=>"badname", :line=>40}, "GoodName"=>{:name=>"GoodName", :line=>41}}} }
   describe '#new' do
     it 'initialize and store names hash as accessor' do
       expect(parser.names).to be_kind_of(Hash)
@@ -11,11 +14,16 @@ describe Parser do
     end
   end
   describe '#extract_names' do
-    it 'should be called on a parser object' do
-      expect(parser.extract_names).to
+    it 'return a hash' do
+      expect(parser.extract_names).to be_kind_of(Hash)
+    end
+    it 'contain all names in a given file' do
+      expect(parser.extract_names).to eq(names_array)
+    end
+    it 'names are categorized into three groups' do
+      expect(parser.extract_names.size).to eq(3)
     end
   end
   describe '#variables_handling' do
-    
   end
 end
