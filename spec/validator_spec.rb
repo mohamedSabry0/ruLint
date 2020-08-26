92433f8817dd62676a03ad6c0cd7579b29f5910f
mohamedSabry0 <mohamedsabry3023@gmail.com>
2020-08-26 13:48:22 +0200

    fix rubocop

--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -4,25 +4,25 @@ describe Validator do
   let(:names) { Parser.new('example.rb').extract_names }
   let(:validator) { Validator.new(names, 'example.rb') }
   let(:warning_messages) do
-    [["naming_warning @(example.rb, line#2):"\
+    [['naming_warning @(example.rb, line#2):'\
       "\n'bad_example' is not a good class/module name. Follow CamelCase naming convention\n\n",
-      "naming_warning @(example.rb, line#5):"\
+      'naming_warning @(example.rb, line#5):'\
       "\n'bad' is not a good class/module name. Follow CamelCase naming convention\n\n",
-      "naming_warning @(example.rb, line#7):"\
+      'naming_warning @(example.rb, line#7):'\
       "\n'Bad_Example' is not a good class/module name. Follow CamelCase naming convention\n\n",
-      "naming_warning @(example.rb, line#15):"\
+      'naming_warning @(example.rb, line#15):'\
       "\n'Mu_Foo' is not a good class/module name. Follow CamelCase naming convention\n\n",
-      "naming_warning @(example.rb, line#20):"\
+      'naming_warning @(example.rb, line#20):'\
       "\n'badname' is not a good class/module name. Follow CamelCase naming convention\n\n"],
-      ["naming_warning @(example.rb, line#23):"\
-      "\n'bad_example' is not a good class/module name. Follow CamelCase naming convention\n\n",
-      "naming_warning @(example.rb, line#26):"\
+     ['naming_warning @(example.rb, line#23):'\
+     "\n'bad_example' is not a good class/module name. Follow CamelCase naming convention\n\n",
+      'naming_warning @(example.rb, line#26):'\
       "\n'bad' is not a good class/module name. Follow CamelCase naming convention\n\n",
-      "naming_warning @(example.rb, line#28):"\
+      'naming_warning @(example.rb, line#28):'\
       "\n'Bad_Example' is not a good class/module name. Follow CamelCase naming convention\n\n",
-      "naming_warning @(example.rb, line#36):"\
+      'naming_warning @(example.rb, line#36):'\
       "\n'Mu_Foo' is not a good class/module name. Follow CamelCase naming convention\n\n",
-      "naming_warning @(example.rb, line#41):"\
+      'naming_warning @(example.rb, line#41):'\
       "\n'badname' is not a good class/module name. Follow CamelCase naming convention\n\n"]]
   end
   describe '#warnings' do
